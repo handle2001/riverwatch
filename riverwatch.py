944893cf63b9938065db1828b259add05291fcff
Josh Benson <handle2001@gmail.com>
2023-12-21 10:46:20 -0500

    Fixed timeSeries comprehension

--- riverwatch.py
+++ riverwatch.py
@@ -8,13 +8,30 @@ import re
 API_URL = "https://waterservices.usgs.gov/nwis/iv/"
 API_RESPONSE_FORMAT = "json"
 
+# USGS Variable Codes:
+# Source: https://help.waterdata.usgs.gov/parameter_cd?group_cd=PHY
+USGS_VARIABLE_CODES = {
+    "00010":"temperature",               # 00010 Temperature, degrees C
+    "00045":"precipitation",             # 00045 Precipitation total, inches
+    "00060":"discharge",                 # 00060 Discharge, cubic ft/s (daily mean)
+    "00065":"height",                    # 00065 Gauge height, ft
+    "00095":"conductance",               # 00095 Specific conductance, uS/cm@25C
+    "00300":"dissolved_oxygen",          # 00300 Dissolved Oxygen, mg/L
+    "00400":"pH",                        # 00400 pH, standard units
+    "63160":"height_above_datum",        # 63160 Water level above NAVD 1988, in feet
+    "63680":"turbidity",                 # 63680 Turbidity, in formazin nephelometric units (FNU)
+}
 # Get list of site codes to query for
 # For now just use "03451500" French Broad at Asheville
 # TODO: Put the list of sites in a file or DB table
 
-sites = ["03451500"]
+sites = [
+    "03451500", # French Broad at Asheville
+    "03441000", # Davidson River at Brevard
+]
 site_data  = {}
 
+    
 # Pull data from USGS for all sites[]
 for site in sites:
     payload = {'format':API_RESPONSE_FORMAT,'sites':site}
@@ -27,34 +44,18 @@ for site in sites:
         print(r.status_code)
 
     raw_data = json.loads(r.text)
-    raw_data = raw_data['value']['timeSeries']
-    site_data[site] = {
-        'siteName': raw_data[0]['sourceInfo']['siteName'],
-        #'timeStamp': raw_data[0]['values'][0]['value'][0]['dateTime'],
-        'temperature': raw_data[0]['values'][0]['value'][0]['value'],
-        'precipitation': raw_data[1]['values'][0]['value'][0]['value'],
-        'discharge': raw_data[2]['values'][0]['value'][0]['value'],
-        'height': raw_data[3]['values'][0]['value'][0]['value'],
-        'conductance': raw_data[4]['values'][0]['value'][0]['value'],
-        'dissolved_oxygen': raw_data[5]['values'][0]['value'][0]['value'],
-        'pH': raw_data[6]['values'][0]['value'][0]['value'],
-        'height_above_datum': raw_data[7]['values'][0]['value'][0]['value'],
-        'turbidity': raw_data[8]['values'][0]['value'][0]['value'],
-    }
-
-# timeSeries
-# 0 = temperature in degrees C
-# 1 = precipitation total in inches
-# 2 = discharge in cubic ft/s
-# 3 = gauge height in feet
-# 4 = specific conductance in microsiemens
-# 5 = dissolved oxygen in mg/L
-# 6 = pH in standard units
-# 7 = stream water level in ft above NAVD 1988
-# 8 = turbidity in formazin nephelometric units
-# Actual value is under value[timeSeries][<index (from above)>][values][value][0][value]
-# Timestamp is under value[timeSeries][0][values][0][value][0][dateTime]
-#     format is YYYY-MM-DDTHH:MM:SS.sss-05:00"
+    timeSeries = raw_data['value']['timeSeries']
+    # The variable code is available in each timeSeries array element under variable->variableCode[0]->value
+    # The actual value of the metric is available under values[0]->value[0]->value
+    # So iterate over the timeSeries[] array, classify the metric type, and grab the value
+    
+    metrics = {}
+    for variable in timeSeries:
+        variableCode = variable['variable']['variableCode'][0]['value']
+        variableValue = variable['values'][0]['value'][0]['value']
+        metrics[USGS_VARIABLE_CODES[variableCode]] = variableValue
+    metrics['siteName'] = timeSeries[0]['sourceInfo']['siteName']
+    site_data[site] = metrics
     
 for site in sites:
     data = site_data[site]
